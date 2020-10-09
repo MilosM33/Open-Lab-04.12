e300628e26411b1daf2a421284dd574d4a6fe7af
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 16:39:33 +0200

    init

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,12 +1,13 @@
-﻿using System;
-
+﻿    using System;
+    using System.Linq;
 namespace Open_Lab_04._12
 {
     public class Numbers
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            var temp =nums.OrderBy(p => p).ToList();
+            return Enumerable.Range(1, temp[temp.Count - 1]+1).Except(temp).ToList()[0];
         }
     }
 }
